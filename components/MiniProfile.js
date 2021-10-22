55a15149214d14ed77c40ed62d366b1fd6b8eea4
Kai <ali.rf.kashani@gmail.com>
2021-10-22 15:36:20 -0400

    miniProfile structure created

--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -1,5 +1,20 @@
 const MiniProfile = () => {
-	return <div></div>
+	return (
+		<div className='flex items-center justify-between mt-14 ml-10'>
+			<img
+				src='/static/images/avatar-1.png'
+				alt=''
+				className='rounded-full border p-[2px] w-16 h-16'
+			/>
+
+			<div className='flex-1 mx-4'>
+				<h2 className='font-bold'>itachi</h2>
+				<h3 className='text-sm text-gray-400'>welcome to instagram</h3>
+			</div>
+
+			<button className='text-blue-400 text-sm font-semibold'>Sign Out</button>
+		</div>
+	)
 }
 
 export default MiniProfile
