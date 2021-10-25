517a72c97b600dcfee617d4f34ab9b104daf5167
Kai <ali.rf.kashani@gmail.com>
2021-10-25 16:34:34 -0400

    updated MiniProfile to use 'user' profile

--- components/Feed.js
+++ components/Feed.js
@@ -1,9 +1,13 @@
+import { useSession } from 'next-auth/client'
+
 import Stories from './Stories'
 import Posts from './Posts'
 import MiniProfile from './MiniProfile'
 import Suggestions from './Suggestions'
 
 function Feed() {
+	const [session, loading] = useSession()
+
 	return (
 		<main className='grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto'>
 			<section className='col-span-2'>
@@ -11,12 +15,14 @@ function Feed() {
 				<Posts />
 			</section>
 
-			<section className='hidden xl:inline-grid md:col-span-1'>
-				<div className='fixed top-20'>
-					<MiniProfile />
-					<Suggestions />
-				</div>
-			</section>
+			{session && (
+				<section className='hidden xl:inline-grid md:col-span-1'>
+					<div className='fixed top-20'>
+						<MiniProfile />
+						<Suggestions />
+					</div>
+				</section>
+			)}
 		</main>
 	)
 }
--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -17,17 +17,19 @@ const MiniProfile = () => {
 	return (
 		<div className='flex items-center justify-between mt-14 ml-10'>
 			<img
-				src='/static/images/avatar-1.png'
+				src={session.user.image}
 				alt=''
 				className='rounded-full border p-[2px] w-16 h-16'
 			/>
 
 			<div className='flex-1 mx-4'>
-				<h2 className='font-bold'>itachi</h2>
+				<h2 className='font-bold'>{session.user.username}</h2>
 				<h3 className='text-sm text-gray-400'>welcome to instagram</h3>
 			</div>
 
-			<button className='text-blue-400 text-sm font-semibold'>Sign Out</button>
+			<button onClick={signOut} className='text-blue-400 text-sm font-semibold'>
+				Sign Out
+			</button>
 		</div>
 	)
 }
